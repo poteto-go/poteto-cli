8bf2acaccc7dac5a26a28280bbc0b310d145f1bf
poteto0 <mahiro.fukuda.aries@gmail.com>
2025-01-09 14:52:18 +0000

    CHANGE: docker template use poteto-cli run

--- template/docker.go
+++ template/docker.go
@@ -6,10 +6,12 @@ FROM golang:1.23
 RUN mkdir app
 WORKDIR /app
 
+RUN go install github.com/poteto-go/poteto-cli/cmd/poteto-cli@latest
+
 COPY go.mod .
 COPY go.sum .
 COPY . .
 RUN go mod download && go mod verify
 
-CMD ["go", "run", "/app/main.go"]
+CMD ["poteto-cli", "run"]
 `
